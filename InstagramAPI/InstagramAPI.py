9caf59d28deda1954677457a4fdc6938d1ba23ad
Raphael Sousa <raphasousa.jau@gmail.com>
2018-02-16 11:50:35 -0200

    Add function to send direct message
    Add function to send direct message

--- InstagramAPI/InstagramAPI.py
+++ InstagramAPI/InstagramAPI.py
@@ -384,7 +384,63 @@ class InstagramAPI:
             except:
                 pass
             return False
-
+    
+    def direct_message(self, text, recipients):
+        if type(recipients) != type([]):
+            recipients = [str(recipients)]
+        recipient_users = '"",""'.join(str(r) for r in recipients)
+        endpoint = 'direct_v2/threads/broadcast/text/'
+        boundary = self.uuid
+        bodies   = [
+            {
+                'type' : 'form-data',
+                'name' : 'recipient_users',
+                'data' : '[["{}"]]'.format(recipient_users),
+            },
+            {
+                'type' : 'form-data',
+                'name' : 'client_context',
+                'data' : self.uuid,
+            },
+            {
+                'type' : 'form-data',
+                'name' : 'thread',
+                'data' : '["0"]',
+            },
+            {
+                'type' : 'form-data',
+                'name' : 'text',
+                'data' : text or '',
+            },
+        ]
+        data = self.buildBody(bodies,boundary)
+        self.s.headers.update (
+            {
+                'User-Agent' : self.USER_AGENT,
+                'Proxy-Connection' : 'keep-alive',
+                'Connection': 'keep-alive',
+                'Accept': '*/*',
+                'Content-Type': 'multipart/form-data; boundary={}'.format(boundary),
+                'Accept-Language': 'en-en',
+            }
+        )
+        #self.SendRequest(endpoint,post=data) #overwrites 'Content-type' header and boundary is missed
+        response = self.s.post(self.API_URL + endpoint, data=data)
+        
+        if response.status_code == 200:
+            self.LastResponse = response
+            self.LastJson = json.loads(response.text)
+            return True
+        else:
+            print ("Request return " + str(response.status_code) + " error!")
+            # for debugging
+            try:
+                self.LastResponse = response
+                self.LastJson = json.loads(response.text)
+            except:
+                pass
+            return False
+        
     def direct_share(self, media_id, recipients, text=None):
         if not isinstance(position, list):
             recipients = [str(recipients)]
